c8be4b7609f967d63457c21f55b8f660243d9fdb
Pierre Jacquier <pierrejacquier39@gmail.com>
2017-03-28 19:49:30 +0200

    Update build_mac.sh

--- build_mac.sh
+++ build_mac.sh
@@ -8,27 +8,27 @@ brew install openssl
 # Build Qt
 cd ~
 
-if [ ! -d "Qt-5.7.0" ]; then
-  mkdir -p Qt-5.7.0
+if [ ! -d "Qt-5.8.0" ]; then
+  mkdir -p Qt-5.8.0
 fi
 
 cd Qt-5.7.0/
 
-if [ ! -f "qt-everywhere-opensource-src-5.7.0.tar.gz" ]; then
+if [ ! -f "qt-everywhere-opensource-src-5.8.0.tar.gz" ]; then
   echo "Getting source files..."
-  wget -N https://download.qt.io/official_releases/qt/5.7/5.7.0/single/qt-everywhere-opensource-src-5.7.0.tar.gz
-  gunzip < qt-everywhere-opensource-src-5.7.0.tar.gz > qt-everywhere-opensource-src-5.7.0.tar
+  wget -N https://download.qt.io/official_releases/qt/5.8/5.8.0/single/qt-everywhere-opensource-src-5.8.0.tar.gz
+  gunzip < qt-everywhere-opensource-src-5.8.0.tar.gz > qt-everywhere-opensource-src-5.8.0.tar
 else
   echo "Already downloaded source files."
 fi
 
-if [ -f "qt-everywhere-opensource-src-5.7.0.tar" ]; then
+if [ -f "qt-everywhere-opensource-src-5.8.0.tar" ]; then
   echo "Extracting downloaded source files..."
-  rm -rf qt-everywhere-opensource-src-5.7.0/
-  tar xf qt-everywhere-opensource-src-5.7.0.tar
+  rm -rf qt-everywhere-opensource-src-5.8.0/
+  tar xf qt-everywhere-opensource-src-5.8.0.tar
 fi
 
-cd qt-everywhere-opensource-src-5.7.0/
+cd qt-everywhere-opensource-src-5.8.0/
 
 echo "Building Qt..."
 chmod +x configure
